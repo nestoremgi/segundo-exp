e3562e1bd738703a817d7133fa0de19fea9a6c69
nestorbfgo <nestor.martinez@bfgo.mx>
2023-08-14 21:37:55 -0600

    Initial commit

---
+++ apps/segundo-exp-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('segundo-exp', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome segundo-exp');
+  });
+});
