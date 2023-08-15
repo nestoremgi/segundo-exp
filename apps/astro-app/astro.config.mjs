d1282a0540d4895629793d1c1f9443e2fddc9b50
nestorbfgo <nestor.martinez@bfgo.mx>
2023-08-14 22:08:16 -0600

    probar server

--- apps/astro-app/astro.config.mjs
+++ apps/astro-app/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: '../../dist/apps/astro-app',
+  output: "server",
+  adapter: vercel()
 });
